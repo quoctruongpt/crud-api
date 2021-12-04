6fc60a7e8655abafba3dc8f9c33c7d2483f0bdfa
Đinh Quốc Trưởng <dinhquoctruong97@gmail.com>
2021-12-04 22:05:17 +0700

    make it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Create from "./components/create";
+import Read from "./components/read";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import Update from "./components/update";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="main text-center">
+        <h2 className="main-header">React Crud Operations</h2>
+      </div>
+      <Switch>
+        <Route path="/create" exact component={Create} />
+        <Route path="/" exact component={Read} />
+        <Route path="/update/:id" exact component={Update} />
+      </Switch>
+    </BrowserRouter>
   );
 }
-
-export default App;
